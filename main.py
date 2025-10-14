3e9f859e57428c751ef49f9c23ff31932940a135
Tim Looijen <tim@lotan.nl>
2025-10-14 21:56:27 +0200

    Experimented with yt-dlp

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from youtube-download-gui!")
+from collections.abc import Collection
+import yt_dlp
 
+def main():
+    a = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ"]
+    yt_download = yt_dlp.YoutubeDL()
+    yt_download.download(a);
 
 if __name__ == "__main__":
     main()
